ae83b5743032e33f79d0bd11b8490196fabf682a
raflialza <muhammadraflialza@gmail.com>
2026-06-04 19:45:55 +0700

    update nih

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -15,7 +15,7 @@ export default async function Home(props: {
   const currentLimit = searchParams.limit
     ? parseInt(searchParams.limit, 10)
     : 10;
-
+  ``;
   return (
     <main className="min-h-screen flex flex-col items-center">
       <div className="flex-1 w-full flex flex-col gap-10 items-center">
